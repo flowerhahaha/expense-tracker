1735d31ce74f4b178dc39118b570353c27dd6e27
Yen <p12427@gmail.com>
2022-08-28 18:59:26 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,38 @@
+// packages and variables
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override') 
+const routes = require('./routes')
+const app = express()
+
+// template engine: express-handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// middleware: static files, body-parser, method-override
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// middleware: session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+  }
+}))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// start the server 
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
