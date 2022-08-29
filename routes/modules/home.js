c2455f98af0cecc74fb66dc87ea18ee55e6eb9de
Yen <p12427@gmail.com>
2022-08-29 14:59:08 +0800

    feat: render homepage by record and category data seeds

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,26 @@
 const router = require('express').Router()
+const dayjs = require('dayjs')
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+const categoryList = require('../../models/seeds/categoryList.json')
+const monthList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 // get homepage
 router.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .populate('categoryId')
+    .sort('-date')
+    .lean()
+    .then(recordList => {
+      let totalAmount = 0
+      const yearList = new Set()
+      recordList.forEach(record => {
+        record.date = dayjs(record.date).format('YYYY-MM-DD')
+        yearList.add(dayjs(record.date).year())
+        totalAmount += record.amount
+      })
+      res.render('index', { categoryList, recordList, yearList, monthList, totalAmount })
+    })
 })
 
 module.exports = router
