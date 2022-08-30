b990e3565b802aebc2ecff6d78319edbb3a3a40d
Yen <p12427@gmail.com>
2022-08-30 10:37:18 +0800

    feat: add partial message.hbs to show flash messages

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,13 @@ router.use('/', home)
 
 // get 404 error page 
 router.get('*', (req, res) => {
-  res.locals.danger_msg = 'The requested URL was not found on this server'
+  res.locals.warning_msg = 'The requested URL was not found on this server'
   res.status(404).render('error')
 })
 
 // 500 error handling
 router.use((err, req, res, next) => {
-  res.locals.danger_msg = 'Sorry! Server is broken. We will fix it soon.'
+  res.locals.warning_msg = 'Sorry! Server is broken. We will fix it soon.'
   console.log(err)
   res.status(500).render('error')
 })
