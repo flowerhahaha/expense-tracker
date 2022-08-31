758b0cad6c297c0ca882133e877548dbefa511ae
Yen <p12427@gmail.com>
2022-08-31 18:02:43 +0800

    feat: send reset password email via SendGrid

---
+++ helpers/email-helpers.js
@@ -0,0 +1,22 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+function sendResentPasswordEmail (clientEmail, link) {
+  return new Promise((resolve, reject) => {
+    const msg = {
+      to: clientEmail,
+      from: process.env.SENDGRID_VERIFIED_SENDER,
+      subject: 'Reset Password for Expense Tracker',
+      html: `<h2>Here is <a href=${link}>your reset password link.</a></h2>
+             <h2>The link is valid for only 10 minutes!</h2>`,
+    }
+    return sgMail
+      .send(msg)
+      .then(response => resolve(response))
+      .catch(e => reject(e))
+  })
+}
+
+module.exports = {
+  sendResentPasswordEmail
+}
--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const passport = require('../../config/passport')
 const jwt = require('jsonwebtoken')
+const User = require('../../models/user')
+const JWT_SECRET = process.env.JWT_SECRET
+const { sendResentPasswordEmail } = require('../../helpers/email-helpers')
 
 router.get('/google', passport.authenticate('google', {
   scope: ['email', 'profile']
@@ -20,8 +23,36 @@ router.get('/facebook/callback', passport.authenticate('facebook', {
   failureRedirect: '/users/login'
 }))
 
+// get forgot-password page
 router.get('/forgot-password', (req, res, nex) => {
   res.render('forgot-password')
+})
+
+// send reset password email
+router.post('/forgot-password', async (req, res, nex) => {
+  try {
+    const { email } = req.body
+    // check if the user exists
+    const user = await User.findOne({ email })
+    if (!user) {
+      res.locals.warning_msg = "Email doesn't exist!"
+      return res.render('forgot-password', { email })
+    }
+    // create a one time link valid for 10 minutes
+    const secret = JWT_SECRET + user.password
+    const payload = {
+      email: user.email,
+      id: user._id
+    }
+    const token = jwt.sign(payload, secret, { expiresIn: '10m' })
+    const link = `http://${req.headers.host}/reset-password/${user._id}/${token}`
+    // sent the link to the email
+    await sendResentPasswordEmail(email, link)
+    res.locals.success_msg = 'Password reset link has been sent to your email.'
+    res.render('forgot-password', { email })
+  } catch (error) {
+    next(e)
+  }
 })
 
 module.exports = router
